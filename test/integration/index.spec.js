5ec26f06861445ab28e434cc23305688fb2f3e1f
Sidorov <sidorov@sportxline.com>
2019-07-26 18:24:44 +0300

    :white_check_mark: Updating integrational tests

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -3,6 +3,8 @@ const cron = require('cron')
 const { ServiceBroker } = require('moleculer')
 const CronJobMixin = require('../../src')
 
+jest.setTimeout(30000)
+
 describe('Test CronJob service', () => {
   const broker = new ServiceBroker({ logger: false })
 
@@ -21,58 +23,121 @@ describe('Test CronJob service', () => {
     }
   }
 
-  afterEach(async () => {
+  beforeEach(async () => {
     onTick.mockClear()
     onComplete.mockClear()
   })
   afterAll(async () => jest.clearAllMocks())
 
-  const service = broker.createService(serviceSchema)
-
-  it('should be ok', async () => {
+  it('should be ok and $cronjob should be the instance of cron.CronJob', async () => {
+    const service = broker.createService(serviceSchema)
     await broker.start()
-
-    expect(service).toBeDefined()
-    expect(service.settings.start).toBe(true)
     expect(service.$cronjob).toBeInstanceOf(cron.CronJob)
-    expect(service.$cronjob.running).toBe(true)
+    await broker.stop()
 
+    await broker.destroyService(service)
+  })
+
+  it('should start two simultaneously', async () => {
+    const service = broker.createService({
+      ...serviceSchema,
+      settings: {
+        withoutOverlapping: false,
+        cronTime: '*/1 * * * * *', // every 1rd secondsd
+        runOnInit: false
+      },
+      methods: {
+        onComplete,
+        onTick: async () => {
+          await new Promise(resolve => setTimeout(resolve, 3000))
+        }
+      }
+    })
+
+    await broker.start()
+    await new Promise(
+      (resolve, reject) => setTimeout(
+        async () => {
+          try {
+            expect(service.$parallelJobsCount).toBe(2)
+            await broker.destroyService(service)
+            await broker.stop()
+            resolve()
+          }
+          catch (err) {
+            await broker.destroyService(service)
+            await broker.stop()
+            reject(err)
+          }
+        }, 2000
+      )
+    )
+  })
+
+  it('should not start two simultaneously', async () => {
+    const service = broker.createService({
+      ...serviceSchema,
+      settings: {
+        withoutOverlapping: true,
+        cronTime: '*/1 * * * * *', // every 1rd secondsd
+        runOnInit: false
+      },
+      methods: {
+        onComplete,
+        onTick: async () => {
+          await new Promise(resolve => setTimeout(resolve, 3000))
+        }
+      }
+    })
+
+    await broker.start()
+    await new Promise(
+      (resolve, reject) => setTimeout(
+        async () => {
+          try {
+            expect(service.$parallelJobsCount).toBe(1)
+            await broker.destroyService(service)
+            await broker.stop()
+            resolve()
+          }
+          catch (err) {
+            await broker.destroyService(service)
+            await broker.stop()
+            reject(err)
+          }
+        }, 2000
+      )
+    )
+  })
+
+  it('should running be false and counter equal to 0', async () => {
+    const service = broker.createService(serviceSchema)
+    await broker.start()
     await broker.stop()
 
     expect(service.$cronjob.running).toBe(false)
     expect(service.$parallelJobsCount).toBe(0)
+
+    await broker.destroyService(service)
+  })
+
+  it('should onComplete be invoked on stop', async () => {
+    const service = broker.createService(serviceSchema)
+    await broker.start()
+    await broker.stop()
+
+    expect(onComplete).toHaveBeenCalledTimes(1)
+
+    await broker.destroyService(service)
+  })
+
+  it('should onTick be invoked on fire', async () => {
+    const service = broker.createService(serviceSchema)
+    await broker.start()
+    await broker.stop()
 
     expect(onTick).toHaveBeenCalledTimes(1)
-    expect(onComplete).toHaveBeenCalledTimes(1)
-  })
 
-  it('should have $cronjob.stop to be invoked if cronjob is running', async () => {
-    await broker.start()
-    const spy = jest.spyOn(service.$cronjob, 'stop')
-    service.$cronjob.stop()
-    service.$cronjob.running = true
-    await broker.stop()
-
-    expect(service.$cronjob.running).toBe(false)
-    expect(service.$parallelJobsCount).toBe(0)
-
-    expect(spy).toHaveBeenCalledTimes(2)
-    spy.mockRestore()
-  })
-
-  it('should have $cronjob.stop to be not invoked if cronjob is not running', async () => {
-    await broker.start()
-    const spy = jest.spyOn(service.$cronjob, 'stop')
-    service.$cronjob.stop()
-    /*
-    service.$cronjob.stop = jest.fn()
-    */
-    await broker.stop()
-
-    expect(service.$cronjob.running).toBe(false)
-    expect(service.$parallelJobsCount).toBe(0)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    spy.mockRestore()
+    await broker.destroyService(service)
   })
 })
