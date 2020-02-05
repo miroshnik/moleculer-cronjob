585be7f85efc8a307bede830c75b4fed120f74fb
Aleksandr Miroshnik <miroshnik@gmail.com>
2020-02-05 12:27:25 +0200

    Update index.spec.js

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -63,8 +63,7 @@ describe('Test CronJob service', () => {
             await broker.destroyService(service)
             await broker.stop()
             resolve()
-          }
-          catch (err) {
+          } catch (err) {
             await broker.destroyService(service)
             await broker.stop()
             reject(err)
